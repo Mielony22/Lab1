78bddc1d199b2cfa692274ae532dcd75f69a530b
Mielony22 <P.mielniczuk99@gmail.com>
2022-03-08 20:15:39 +0100

    Pierwsze zasilenie

---
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -0,0 +1,21 @@
+package pl.lublin.wsei.java.cwiczenia;
+//jak w przeglądarce projektu, definicja klasy public class Main
+
+
+public class Main {
+    /*- nazwa klasy taka sama jak
+nazwa pliku Main.java */
+
+    public static void main(String[] args) {
+        System.out.print("Ala");
+        System.out.print("Ma");
+        System.out.print("Kota\n");
+        System.out.print("Ala\n");
+        System.out.print("Ma\n");
+        System.out.print("Kota\n");
+        int a =3;
+        double b = 4.21;
+        String s = "Ala ma kolejnego kota";
+        System.out.printf("a = %d, b = %.2f, s = %20s %n", a,b,s);
+    }
+}
