456e2da7650676e1130a25a3512704eeecfac44f
Mielony22 <P.mielniczuk99@gmail.com>
2022-03-15 13:01:19 +0100

    Pierwsze zasilenie

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,6 +2,8 @@ package pl.lublin.wsei.java.cwiczenia;
 //jak w przeglądarce projektu, definicja klasy public class Main
 
 
+import java.util.Scanner;
+
 public class Main {
     /*- nazwa klasy taka sama jak
 nazwa pliku Main.java */
@@ -13,9 +15,26 @@ nazwa pliku Main.java */
         System.out.print("Ala\n");
         System.out.print("Ma\n");
         System.out.print("Kota\n");
-        int a =3;
+        int a = 3;
         double b = 4.21;
         String s = "Ala ma kolejnego kota";
-        System.out.printf("a = %d, b = %.2f, s = %20s %n", a,b,s);
+        System.out.printf("a = %d, b = %.2f, s = %20s %n", a, b, s);
+        System.out.print("Nazywalismy to \"witamina B3\n");
+        System.out.print("alfa\tsin(alfa)\n");
+        for (int i = 0; i < 370; i += 10)
+            System.out.printf("%d\t%f\t\n", i, Math.sin(i / 360.0 * 2 * Math.PI));
+        Scanner input = new Scanner(System.in);
+        int num1, num2=0;
+        do {
+            System.out.print("Podaj pierwsza liczbę:");
+            num1 = input.nextInt();
+            if (num1 != 0) {
+                System.out.print("Podaj drugą liczbę:");
+                num2 = input.nextInt();
+            }
+            System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
+        } while ((num1 != 0) && (num2 != 0));
+
+
     }
 }
