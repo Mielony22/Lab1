7d9e3d8bd38553d7056828756fdf703c75f0e0d6
Mielony22 <P.mielniczuk99@gmail.com>
2022-03-15 22:35:39 +0100

    Typy tablicowe

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,23 +2,31 @@ package pl.lublin.wsei.java.cwiczenia;
 //jak w przeglądarce projektu, definicja klasy public class Main
 
 
-import java.util.Scanner;
+
+import java.util.Random;
+
+
+
 
 public class Main {
-    /*- nazwa klasy taka sama jak
-nazwa pliku Main.java */
 
     public static void main(String[] args) {
+        int[] liczby = new int[30];
+        Random rnd = new Random();
 
-        Scanner input = new Scanner(System.in);
-        int num1, num2;
-        do {
-            System.out.print("Podaj liczbe, ktore mam dodac:");
-            num1 = input.nextInt();
-            num2 = input.nextInt();
-            if ((num1 == 0) || (num2 == 0)) break;
-            System.out.printf("Wynik dodawania %d +%d = %d%n", num1, num2, num1 + num2);
-        } while (true);
+        for(int i =0; i <30; i++)
+        liczby[i] = rnd.nextInt();
+        int mn = Integer.MAX_VALUE;
+        long avg = 0;
+        int mx = Integer.MIN_VALUE;
+        for (int l : liczby)
+        {
+            System.out.println(1);
+            if (l <mn) mn =l;
+            if (l> mx) mx =l;
+            avg += 1;
+        }
+System.out.printf("MIN = %d, MAX = %d, AVG =%f", mn,mx, (float)avg/liczby.length);
 
 
     }
